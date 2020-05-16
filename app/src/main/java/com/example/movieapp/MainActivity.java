62b31d6c83aadb352016f716cc233d10bbe2756f
Akhil James Joseph <akhiljamesjoseph944@gmail.com>
2020-05-16 19:59:47 -0400

    pure di done for all views Next: setup observer listener

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -6,11 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
+import com.example.movieapp.BaseClasses.BaseActivity;
+import com.example.movieapp.PureDI.MVCViewFactory;
 import com.example.movieapp.movieList.MovieListMVC;
 import com.example.movieapp.movieList.MovieListMVCInterface;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     RecyclerView recyclerView;
     LayoutInflater layoutInflater;
@@ -18,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        layoutInflater = LayoutInflater.from(this);
-        MovieListMVCInterface movieListMVCInterface = new MovieListMVC(layoutInflater,null,this);
+        MVCViewFactory viewfactory = getCompositionRoot().getMVCViewFactory();
+        MovieListMVCInterface movieListMVCInterface = viewfactory.getMovieListMVC(null);
         setContentView(movieListMVCInterface.getRootView());
 
 
--- app/src/main/java/com/example/movieapp/movieList/MovieListMVC.java
+++ app/src/main/java/com/example/movieapp/movieList/MovieListMVC.java
@@ -3,27 +3,30 @@ package com.example.movieapp.movieList;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.movieapp.PureDI.MVCViewFactory;
 import com.example.movieapp.R;
 import com.example.movieapp.views.BaseObservableViewMvc;
 import com.example.movieapp.views.ObservableViewMvc;
 
 public class MovieListMVC extends BaseObservableViewMvc<MovieListMVCInterface.Listener> implements MovieListMVCInterface{
 
-    LayoutInflater layoutInflater;
-    RecyclerView recyclerView;
 
-    public MovieListMVC(LayoutInflater layoutInflater, ViewGroup viewGroup, Context context) {
+    private RecyclerView recyclerView;
+
+
+    public MovieListMVC(LayoutInflater layoutInflater, ViewGroup viewGroup, MVCViewFactory viewFactory) {
 
         setRootView(layoutInflater.inflate(R.layout.activity_main,viewGroup));
 
         recyclerView = findViewById(R.id.movieList);
-        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(layoutInflater);
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(layoutInflater,viewFactory);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(recyclerAdapter);
 
 
